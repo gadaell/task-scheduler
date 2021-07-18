a2c3434ae58a306d8f6b0684acd9622966cbdb28
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-07-18 09:41:16 -0500

    finished project

--- Develop/main.js
+++ Develop/main.js
@@ -1,40 +1,7 @@
-// //Date Code
-// const d = new Date();
-
-// const months = [
-//   "January",
-//   "February",
-//   "March",
-//   "April",
-//   "May",
-//   "June",
-//   "July",
-//   "August",
-//   "September",
-//   "October",
-//   "November",
-//   "December",
-// ];
-// const days = [
-//   "Sunday,",
-//   "Monday,",
-//   "Tuesday,",
-//   "Wednesday,",
-//   "Thursday,",
-//   "Friday,",
-//   "Saturday,",
-// ];
-// document.getElementById("day").innerHTML = days[d.getDay()];
-// document.getElementById("date").innerHTML = d.getDate();
-// document.getElementById("month").innerHTML = months[d.getMonth()];
-// document.getElementById("year").innerHTML = d.getFullYear();
-
-//Date Code
 var times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 var currentTime = moment().hours();
 var currentDay = document.getElementById("currentDay");
 var hourDescription = $(".description");
-console.log(hourDescription);
 var text = document.createTextNode(
   moment().format("dddd") + ", " + moment().format("MMMM-DD-YYYY")
 );
@@ -45,15 +12,11 @@ if (times.includes(moment().format("hh A"))) {
   var index = times.indexOf(timestring);
   var timeBlock = timestring.substring(0, 2) + "block";
 }
-console.log(document.getElementsByClassName("description"));
-
-console.log(hourDescription);
 
 //change color on rows
 changeRowColors = function () {
   $(".time-block").each(function () {
     var timeBlockHour = parseInt($(this).attr("data-id"));
-    console.log(timeBlockHour);
 
     if (timeBlockHour == currentTime) {
       $(this).addClass("present");
@@ -65,11 +28,50 @@ changeRowColors = function () {
   });
 };
 changeRowColors();
-// document
-//   .getElementById(timeBlock)
-//   .getElementsByClassName("hour")
-//   .remove("past");
-// document
-//   .getElementById(timeBlock)
-//   .getElementsByClassName("hour")
-//   .remove("future");
+// button functioning
+var taskTextInput = [];
+$("button").click(function () {
+  var taskTextInput = [
+    $("#text01").val(),
+    $("#text02").val(),
+    $("#text03").val(),
+    $("#text04").val(),
+    $("#text05").val(),
+    $("#text06").val(),
+    $("#text07").val(),
+    $("#text08").val(),
+    $("#text09").val(),
+  ];
+  // local storage function
+  localStorage.setItem("taskTextInput", JSON.stringify(taskTextInput));
+});
+// Load Tasks Storage function.
+loadText = function () {
+  // variable used to get and store the taskTextInput key values from local storage.
+  var loadUserTasks = JSON.parse(localStorage.getItem("taskTextInput"));
+  //Created storedTaskInputEl Variable and linked them to the ElementID "Text01, 02, 03, etc" and
+  //matched them w/ the corresponding index for the loadUserTasks.
+  var storedTaskInputEl = document.getElementById("text01");
+  storedTaskInputEl.textContent = loadUserTasks[0];
+  var storedTaskInputEl = document.getElementById("text02");
+  storedTaskInputEl.textContent = loadUserTasks[1];
+  var storedTaskInputEl = document.getElementById("text03");
+  storedTaskInputEl.textContent = loadUserTasks[2];
+  var storedTaskInputEl = document.getElementById("text04");
+  storedTaskInputEl.textContent = loadUserTasks[3];
+  var storedTaskInputEl = document.getElementById("text05");
+  storedTaskInputEl.textContent = loadUserTasks[4];
+  var storedTaskInputEl = document.getElementById("text06");
+  storedTaskInputEl.textContent = loadUserTasks[5];
+  var storedTaskInputEl = document.getElementById("text07");
+  storedTaskInputEl.textContent = loadUserTasks[6];
+  var storedTaskInputEl = document.getElementById("text08");
+  storedTaskInputEl.textContent = loadUserTasks[7];
+  var storedTaskInputEl = document.getElementById("text09");
+  storedTaskInputEl.textContent = loadUserTasks[8];
+};
+
+// window onload command to call the loadTasks function upon refresh.
+window.onload = function () {
+  loadText();
+};
